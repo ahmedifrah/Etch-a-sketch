0239b139475123342ef52f5549aecd11c06fd6b4
ahmedifrah <ahmed.ifrah.ibrahim@gmail.com>
2022-11-08 15:05:32 +0500

    add 256 divs to make 16x16 grid layout

---
+++ script.js
@@ -0,0 +1,7 @@
+const container =  document.querySelector('.container');
+for (let i = 1; i < 257; i++){
+    const div = document.createElement('div');
+    div.textContent = ('hello');
+    container.appendChild(div);
+}
+
