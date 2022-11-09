e3127704c8c8157c25f840e89fcb5e724a12a805
ahmedifrah <ahmed.ifrah.ibrahim@gmail.com>
2022-11-09 14:19:14 +0500

    add function to remove and change grid size

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const container =  document.querySelector('.container');
+const button = document.querySelector('.size');
+let numInputSize = 256;
+button.addEventListener('click', changeSize);
 
 function makeGrid() {
-    for (let i = 1; i < 257; i++){
+    for (let i = 1; i < (numInputSize)+1; i++){
         const div = document.createElement('div');
-        div.classList.add(`box${i}`);
-        div.textContent = ('hello');
+        div.classList.add('box');
+        div.textContent = (`${i}`);
         div.addEventListener('mouseover', () => {
             div.style.backgroundColor = "grey";
         });
@@ -13,10 +16,32 @@ function makeGrid() {
 
 }
 
+function changeSize(){
+    removeGrid();
+    getSize();
+    container.style.gridTemplateColumns = `repeat(${getSize}, 1fr)`;
+    makeGrid();
+    document.querySelector('.resolution').textContent = `${numInputSize}x${numInputSize}`;
+ 
+
+
+}
+function removeGrid(){
+    document.querySelectorAll('.box').forEach(e => e.remove());
+}
+
+function getSize(){
+    const inputSize = prompt('Size of grid');
+    numInputSize = parseInt(inputSize);
+    console.log(numInputSize);
+    removeGrid();
+    return numInputSize;
+}
+
+
+
 // function changeBoxColor(){
 //     div.style.backgroundColor = "red"
 // }
 
 makeGrid();
-
-
