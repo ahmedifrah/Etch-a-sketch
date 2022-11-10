d244337e79986a25a4595fe0d1994ae1197e1d3f
ahmedifrah <ahmed.ifrah.ibrahim@gmail.com>
2022-11-10 13:11:07 +0500

    add logic for rainbow and toggleLines button

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const container =  document.querySelector('.container');
-const button = document.querySelector('.size');
+const createNewButton = document.querySelector('.size');
+const rainbow = document.querySelector('.rainbow');
+const lines = document.querySelector('.lines');
+const shade = document.querySelector('.shade');
 let numInputSize = 256;
-button.addEventListener('click', changeSize);
+createNewButton.addEventListener('click', changeSize);
+rainbow.addEventListener('click', changeRainbow);
+lines.addEventListener('click', toggleLines)
+
 
 function makeGrid() {
     for (let i = 1; i < 257; i++){
         const div = document.createElement('div');
         div.classList.add('box');
-        // div.textContent = (`${i}`);
         div.addEventListener('mouseover', () => {
             div.style.backgroundColor = "grey";
         });
@@ -21,19 +26,45 @@ function changeSize(){
     container.style.gridTemplateColumns = `repeat(${numInputSize}, 1fr)`;
     removeGrid();
     createNew();
-    document.querySelector('.resolution').textContent = `${numInputSize}x${numInputSize}`;
+    if(!isNaN(numInputSize)){
+        document.querySelector('.resolution').textContent = `${numInputSize}x${numInputSize}`;
+    }
+    
  
-
-
 }
+function changeRainbow(){
+    getSize();
+    container.style.gridTemplateColumns = `repeat(${numInputSize}, 1fr)`;
+    removeGrid();
+    createRainbow();
+    if(!isNaN(numInputSize)){
+        document.querySelector('.resolution').textContent = `${numInputSize}x${numInputSize}`;
+    }
+}
+
+
+
 function removeGrid(){
     document.querySelectorAll('.box').forEach(e => e.remove());
+    
 }
 
 function getSize(){
     const inputSize = prompt('Size of grid');
     numInputSize = parseInt(inputSize);
-    return numInputSize;
+    if (isNaN(numInputSize) || (numInputSize < 1 || numInputSize > 100)){
+        const error = document.createElement('div');
+        const body = document.querySelector('body');
+        error.textContent = "Please input a number";
+        error.style.fontSize = "30px";
+        error.style.fontWeight = "bold";
+        error.style.textAlign = "center";
+    
+        body.append(error);
+    } else {
+        return numInputSize;
+    }
+    
 }
 
 function createNew(){
@@ -45,6 +76,28 @@ function createNew(){
         });
         container.appendChild(div);
     }
+}
+
+function createRainbow(){
+    for (let i = 1; i < (numInputSize*numInputSize)+1; i++){
+        const div = document.createElement('div');
+        div.classList.add('box');
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        console.log(randomColor);
+
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = `#${randomColor}`;
+        });
+        container.appendChild(div);
+    }
+}
+
+
+
+function toggleLines(){
+    const divBorder = document.querySelectorAll('.box');
+    divBorder.forEach((div) => div.style.border = div.style.border === 'none' ? '' : 'none');
+        
 }
 
 
